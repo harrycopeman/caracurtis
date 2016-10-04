c2aa36643f1961a47931841fa87a570aaff5b588
Harry Copeman <harry.copeman@gmail.com>
2016-10-04 18:53:40 +0100

    First Push

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$title = "Cara Curtis";
+include ("_include/header.php");
+?>
+
+<section class="full">
+	<div class="span">
+		<div class="loop-12">
+
+			<intro>
+				<img src="_core/imgs/me.jpg">
+				<h1>Cara Curtis</h1>
+			</intro>
+
+			<p>Hi, I'm Cara - currently based in London with a love for travelling, cycling and eighties movies.<br><br>
+			I lead social media and content at <a target="_blank" href="http://journal.lovemebeauty.com/">Love Me Beauty</a>, where I provide the creative outlook, imagery and good coffee. I'm also busy studying BA Journalism at London Metropolitan Univeristy in my second year.<br><br>
+			Feel free to send me an <a href="mailto:carac97@hotmail.com">email</a>.
+
+			 </p>
+
+
+
+			</div>
+		</div>
+	</div>
+
+				<div class="insta">
+
+			 <!-- LightWidget WIDGET --><script src="//lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/896f718cac3e547c99124a9f1c7cddfe.html" id="lightwidget_896f718cac" name="lightwidget_896f718cac"  scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+
+			</div>
+
+
+
+
+</section>
+
+
+<?php include ("_include/footer.php"); ?>
