27250dafb43d3585b10d49254a397ce94ad6ee47
Harry Copeman <harry.copeman@gmail.com>
2018-01-30 20:41:11 +0000

    New site

--- _include/header.php
+++ _include/header.php
@@ -4,13 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1"/>
     <link rel="shortcut icon" href="/favicon.png"/>
-    <link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet' type='text/css'>
 
   	<title><?php echo $title; ?></title>
-  	<link href='/_core/scss/style.css' rel='stylesheet'>
-
-    <script src="https://use.typekit.net/vve7mtx.js"></script>
-	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+  	<link href='/_core/css/style.css' rel='stylesheet'>
 
     </script>
 
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+$title = "Cara Curtis";
+$id = "homepage";
+include ("_include/header.php");
+?>
+
+
+<div class="span">
+	<div class="loop-12">
+		<div class="loop-12">
+			<h1 class="intro">Hello, I’m Cara Curtis, a writer living in London</h1>
+			<h2>Here is some of my work.</h2>
+		</div>
+	</div>
+</div>
+
+<div class="span">
+	<div class="loop-12">
+
+		<div id="one" class="loop-6">
+			<p class="work-title">1</p>
+			<div class="work-container">
+				<img src="_core/imgs/example.png">
+			</div>
+		</div>
+
+		<div id="two" class="loop-6">
+			<p class="work-title">2</p>
+			<div class="work-container">
+				<img src="_core/imgs/example.png">
+			</div>
+		</div>
+
+		<div id="three" class="loop-6">
+			<p class="work-title">3</p>
+			<div class="work-container">
+				<img src="_core/imgs/example.png">
+			</div>
+		</div>
+
+		<div class="loop-6">
+			<p class="work-title">4</p>
+			<div class="work-container">
+				<img src="_core/imgs/example.png">
+			</div>
+		</div>
+
+
+
+	</div>
+</div>
+
+
+</body>
+
+
+<?php include ("_include/footer.php"); ?>
